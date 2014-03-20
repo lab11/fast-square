c6d187ce9c443ebe8b385416fa24efa8acca8e3e
Ben Kemkpe <bpkempke@umich.edu>
2014-03-20 20:01:46 +0000

    forgot to account for the two cascaded comb filters

--- src/matlab/correctCOMBPhase.m
+++ src/matlab/correctCOMBPhase.m
@@ -2,4 +2,5 @@
 comb_phase = phasez(1,[1,0,0,0,0,0,0,0.875],2*pi*harmonic_freqs(:)/sample_rate);
 comb_phase = reshape(comb_phase,size(harmonic_freqs));
 
-square_phasors = square_phasors.*exp(-1i*repmat(shiftdim(comb_phase,-1),[size(anchor_positions,1),1,1]));
+%Factor of two comes from the two cascaded comb filters
+square_phasors = square_phasors.*exp(-1i*2*repmat(shiftdim(comb_phase,-1),[size(anchor_positions,1),1,1]));
