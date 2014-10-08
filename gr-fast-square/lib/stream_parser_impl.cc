909f96e794512382d8ab01a48038a0cae73e4004
Ben Kempke <bpkempke@umich.edu>
2014-10-08 17:32:28 +0000

    decided to just separate the two methods for now

--- gr-fast-square/lib/stream_parser_impl.cc
+++ gr-fast-square/lib/stream_parser_impl.cc
@@ -20,11 +20,11 @@ freq_stitcher::sptr freq_stitcher::make(int message_id_base){
 stream_parser_impl::stream_parser_impl(int message_id_base)
 	: sync_block("stream_parser",
 			io_signature::make(4, 4, sizeof(gr_complex)),
-			io_signature::make(4, 4, FFT_SIZE*sizeof(gr_complex))),
+			io_signature::make(0, 0, sizeof(gr_complex))),
 	d_packet_id(message_id_base)
 {
-//	//Register message port for outgoing data packets
-//	message_port_register_out(pmt::mp("sequence_data_out"));
+	//Register message port for outgoing data packets
+	message_port_register_out(pmt::mp("sequence_data_out"));
 
 	sequence_nums = new int[4];
 	d_cur_sequence = 0;
@@ -34,18 +34,12 @@ stream_parser_impl::stream_parser_impl(int message_id_base)
 stream_parser_impl::~stream_parser_impl(){
 }
 
-void ccsds_tm_conv_decoder_impl::forecast(int noutput_items, gr_vector_int &ninput_items_required){
-	for(int ii=0; ii < ninput_items_required.size(); ii++){
-			ninput_items_required[ii] = noutput_items;
-	}
-}
-
 int stream_parser_impl::work(int noutput_items,
-		gr_vector_int &ninput_items,
 		gr_vector_const_void_star &input_items,
 		gr_vector_void_star &output_items){
 
 	gr_complex *out = (gr_complex *) output_items[0];
+	int count=0;
 	int out_count = 0;
 
 	//Loop over all anchors
@@ -53,7 +47,7 @@ int stream_parser_impl::work(int noutput_items,
 		const gr_complex *in = (const gr_complex *) input_items[ii];
 
 		//Loop over all new data
-		for(int jj=0; jj < ninput_items[ii]; jj++){
+		for(int jj=0; jj < noutput_items; jj++){
 			data_history[ii].push(in[jj]);
 		}
 	}
@@ -62,52 +56,45 @@ int stream_parser_impl::work(int noutput_items,
 	bool snapshot_flag = true;
 	uint32_t hsn = 0; //hsn = highest sequence num
 	int hsn_idx = 0;
-	while(snapshot_flag){
-		for(int ii=0; ii < input_items.size();){
-			while(data_history[ii].size() > SAMPLES_PER_SEQ && data_history[ii][SAMPLES_PER_SEQ].imag() == -1.0){
-				data_history[ii].pop_front();
-			}
-			//Check to see if there is enough data for a full snapshot.
-			if(data_history[ii].size() <= SAMPLES_PER_SEQ){
-				snapshot_flag = false;
-				break;
-			} else {
-				uint32_t sequence_num = getSequenceNum(data_history[ii][SAMPLES_PER_SEQ-1]);
-	
-				//In case a sequence number has been skipped, delete any stale data
-				if(sequence_num > hsn && ii > 0){
-					data_history[hsn_idx].erase(data_history[hsn_idx].begin(), data_history[hsn_idx].begin()+SAMPLES_PER_SEQ);
-					ii = 0;
-					continue;
-				} else if(sequence_num < hsn){
-					data_history[ii].erase(data_history[ii].begin(), data_history[ii].begin()+SAMPLES_PER_SEQ);
-					ii = 0;
-					continue;
-				}
-			}
-			ii++;
+	for(int ii=0; ii < input_items.size();){
+		while(data_history[ii].size() > SAMPLES_PER_SEQ && data_history[ii][SAMPLES_PER_SEQ].imag() == -1.0){
+			data_history[ii].pop_front();
 		}
-	
-		//If snapshot_flag is set, it means we have a full snapshot and all data is aligned in data_history
-		if(snapshot_flag){
-			for(int ii=0; ii < input_items.size(); ii++){
-				memcpy(&output_items[ii][0], &data_history[ii][0], FFT_SIZE*sizeof(gr_complex));
+		//Check to see if there is enough data for a full snapshot.
+		if(data_history[ii].size() <= SAMPLES_PER_SEQ){
+			snapshot_flag = false;
+			break;
+		} else {
+			uint32_t sequence_num = getSequenceNum(data_history[ii][SAMPLES_PER_SEQ-1]);
+
+			//In case a sequence number has been skipped, delete any stale data
+			if(sequence_num > hsn && ii > 0){
+				data_history[hsn_idx].erase(data_history[hsn_idx].begin(), data_history[hsn_idx].begin()+SAMPLES_PER_SEQ);
+				ii = 0;
+				continue;
+			} else if(sequence_num < hsn){
+				data_history[ii].erase(data_history[ii].begin(), data_history[ii].begin()+SAMPLES_PER_SEQ);
+				ii = 0;
+				continue;
 			}
-			out_count++;
-	
-			////Prepare an outgoing message containing all data
-			//pmt::pmt_t new_message_dict = pmt::make_dict();
-			//for(int ii=0; ii < input_items.size(); ii++){
-			//	pmt::pmt_t key = pmt::from_long((long)(d_packet_id+ii));
-			//	pmt::pmt_t value = pmt::init_c32vector(SAMPLES_PER_SEQ, &data_history[hsn_idx][0]);
-			//	new_message_dict = pmt::dict_add(new_message_dict, key, value);
-			//}
-			//pmt::pmt_t new_message = pmt::cons(new_message_dict, pmt::PMT_NIL);
-			//message_port_pub(pmt::mp("sequence_data_out"), new_message);
 		}
+		ii++;
 	}
 
-	return out_count;
+	//If snapshot_flag is set, it means we have a full snapshot and all data is aligned in data_history
+	if(snapshot_flag){
+		//Prepare an outgoing message containing all data
+		pmt::pmt_t new_message_dict = pmt::make_dict();
+		for(int ii=0; ii < input_items.size(); ii++){
+			pmt::pmt_t key = pmt::from_long((long)(d_packet_id+ii));
+			pmt::pmt_t value = pmt::init_c32vector(SAMPLES_PER_SEQ, &data_history[hsn_idx][0]);
+			new_message_dict = pmt::dict_add(new_message_dict, key, value);
+		}
+		pmt::pmt_t new_message = pmt::cons(new_message_dict, pmt::PMT_NIL);
+		message_port_pub(pmt::mp("sequence_data_out"), new_message);
+	}
+
+	return noutput_items;
 }
 
 uint32_t stream_parser_impl::getSequenceNum(gr_complex data){
--- gr-fast-square/lib/stream_parser_ports_impl.h
+++ gr-fast-square/lib/stream_parser_ports_impl.h
@@ -20,7 +20,9 @@ public:
 	stream_parser_impl(int message_id_base);
 	~stream_parser_impl();
 
-	int work(int noutput_items,
+	void forecast(int noutput_items, gr_vector_int &ninput_items_required);
+	int general_work(int noutput_items,
+			gr_vector_int &ninput_items,
 			gr_vector_const_void_star &input_items,
 			gr_vector_void_star &output_items);
 };
