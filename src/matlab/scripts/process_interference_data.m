52dfe3de4448d58c05278022c9948d69f0601d7d
Ben Kempke <bpkempke@umich.edu>
2015-10-11 22:18:42 -0400

    adding ability to processes narrowband interference datasets

---
+++ src/matlab/scripts/process_interference_data.m
@@ -0,0 +1,7 @@
+addpath('~/repos/fast-square/src/matlab/');
+addpath('~/repos/fast-square/src/matlab/bjt');
+
+cd ~/temp/diversity_data/ipsn16/stationary_tee_interference/
+analyzeHSCOMBData_bjt('operation','localization','system_setup','diversity','anchor',3,'ignore_freq_steps',[23,24]);
+%analyzeHSCOMBData_bjt('operation','diversity_localization','system_setup','diversity');
+%analyzeHSCOMBData_bjt('operation','post_localization','system_setup','diversity');
