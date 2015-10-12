3b0bf550e7ebe82106dc19df5005bfecb14c57e0
Ben Kempke <bpkempke@umich.edu>
2015-10-12 15:48:42 -0400

    compressive sensing portion seems to now work

--- src/matlab/harmonicLocalization_r7.m
+++ src/matlab/harmonicLocalization_r7.m
@@ -15,35 +15,26 @@ tx_phasors_reshaped = reshape(tx_phasors_reshaped,[size(tx_phasors_reshaped,1),s
 tx_phasors_reshaped = [tx_phasors_reshaped(:,133:end),tx_phasors_reshaped(:,1:132)];
 
 %Identify pure-zero phasors (they should be from zeroing due to narrowband cancellation)
-N = size(tx_phasors_reshaped,2);
-for hl_ii = 1:size(tx_phasors_reshaped,1)
-	for hl_jj = 1:2
-		if(hl_jj == 1)
-			x = real(tx_phasors_reshaped(hl_ii,:));
-		else
-			x = imag(tx_phasors_reshaped(hl_ii,:));
-		end
-
-		OMEGA = find(x > 0);
-		OMEGA_NOT = find(x == 0);
-		N = length(x);
-		K = sum(x > 0);
-		A = randn(K,N);
-		A = orth(A')';
-		y = A*x;
-		x0 = A'*y;
-		xp = l1eq_pd(x0, A, [], y, 1e-3);
-
-		if(hl_jj == 1)
-			x_real = xp;
-		else
-			x_imag = xp;
-		end
+if(length(res.ignore_freq_steps) > 0)
+	N = size(tx_phasors_reshaped,2);
+	for hl_ii = 1:size(tx_phasors_reshaped,1)
+		y = square_phasors_reshaped(hl_ii,:).';
+	
+		OMEGA = find(abs(y) > 0);
+		OMEGA_NOT = find(abs(y) == 0);
+		N = length(y);
+		K = sum(abs(y) > 0);
+		B=dftmtx(N);
+		Binv=inv(B);
+		A=B(OMEGA,:);
+		y = y(OMEGA);
+		x0=A'*y;
+		xp=l1eq_pd(x0,A,[],y,1e-5);
+	
+		new_cir = xp;
+		new_fr = fft(new_cir);
+		tx_phasors_reshaped(hl_ii,OMEGA_NOT) = new_fr(OMEGA_NOT);
 	end
-	new_cir = x_real + 1i*x_imag;
-	new_fr = fft(new_cir);
-	tx_phasors_reshaped(hl_ii,OMEGA_NOT) = new_fr(OMEGA_NOT);
-	keyboard;
 end
 
 %Calculate ToAs and the corresponding impulse response
--- src/matlab/scripts/process_interference_obstructed_data.m
+++ src/matlab/scripts/process_interference_obstructed_data.m
@@ -1,8 +1,7 @@
 addpath('~/repos/fast-square/src/matlab/');
 addpath('~/repos/fast-square/src/matlab/bjt');
-addpath('~/repos/fast-square/src/matlab/l1magic/Optimization');
 
-cd ~/temp/diversity_data/ipsn16/stationary_tee_interference/
+cd ~/temp/diversity_data/ipsn16/stationary_tee_interference_obstructed/
 analyzeHSCOMBData_bjt('operation','localization','system_setup','diversity','anchor',3,'ignore_freq_steps',[23,24]);
 %analyzeHSCOMBData_bjt('operation','diversity_localization','system_setup','diversity');
 %analyzeHSCOMBData_bjt('operation','post_localization','system_setup','diversity');
